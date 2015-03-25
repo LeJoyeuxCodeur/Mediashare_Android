a492395f0d2c66ec975dcfbf93fc4efd6e625e9b
ALEXANDRE CUVILLIER <alexandre.cuvillier@etudiant.univ-lille1.fr>
2015-03-25 10:43:21 +0100

    ajout des fichiers

---
+++ app/src/main/java/fr/univ_lille1/iut_info/vergauwb/mediashare_android/Contact.java
@@ -0,0 +1,66 @@
+package fr.univ_lille1.iut_info.vergauwb.mediashare_android;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contact extends Activity{
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        setContentView(R.layout.contact);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+     public void connexion(View v){
+        startActivity(new Intent(Contact.this, Connexion.class));
+    }
+
+    public void inscription(View v){
+        startActivity(new Intent(Contact.this, Inscription.class));
+    }
+
+    public void accueil(View v){
+        startActivity(new Intent(Contact.this, MainActivity.class));
+    }
+}
